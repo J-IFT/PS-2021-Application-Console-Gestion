e5196a951c34f3a4d7a039ae5525c48a48fd391a
juliette-ift <juliette.infanti@gmail.com>
2021-09-27 11:02:51 +0200

    premier commit

---
+++ ProjetJulietteJeremyMailys/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetJulietteJeremyMailys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
